03254585449aca603411cdcc2d2a31bdbb84173f
728 <septem.duo.octo@outlook.com>
2022-12-10 14:42:32 +0800

    dbimport #32

--- cli/dbimport.zsh
+++ cli/dbimport.zsh
@@ -1,23 +1,23 @@
-echo mongoimport --jsonArray -d=ismism -c=user --file=dbimport/user.json
-mongoimport --jsonArray -d=ismism -c=user --file=dbimport/user.json
+echo mongoimport --jsonArray -d=ismism -c=user --mode=upsert --file=dbimport/user.json
+mongoimport --jsonArray -d=ismism -c=user --mode=upsert --file=dbimport/user.json
 
-echo mongoimport --jsonArray -d=ismism -c=soc --file=dbimport/soc.json
-mongoimport --jsonArray -d=ismism -c=soc --file=dbimport/soc.json
+echo mongoimport --jsonArray -d=ismism -c=soc --mode=upsert --file=dbimport/soc.json
+mongoimport --jsonArray -d=ismism -c=soc --mode=upsert --file=dbimport/soc.json
 
-echo mongoimport --jsonArray -d=ismism -c=agenda --file=dbimport/agenda.json
-mongoimport --jsonArray -d=ismism -c=agenda --file=dbimport/agenda.json
+echo mongoimport --jsonArray -d=ismism -c=agenda --mode=upsert --file=dbimport/agenda.json
+mongoimport --jsonArray -d=ismism -c=agenda --mode=upsert --file=dbimport/agenda.json
 
-echo mongoimport --jsonArray -d=ismism -c=worker --file=dbimport/worker.json
-mongoimport --jsonArray -d=ismism -c=worker --file=dbimport/worker.json
+echo mongoimport --jsonArray -d=ismism -c=worker --mode=upsert --file=dbimport/worker.json
+mongoimport --jsonArray -d=ismism -c=worker --mode=upsert --file=dbimport/worker.json
 
-echo mongoimport --jsonArray -d=ismism -c=work --file=dbimport/work.json
-mongoimport --jsonArray -d=ismism -c=work --file=dbimport/work.json
+echo mongoimport --jsonArray -d=ismism -c=work --mode=upsert --file=dbimport/work.json
+mongoimport --jsonArray -d=ismism -c=work --mode=upsert --file=dbimport/work.json
 
-echo mongoimport --jsonArray -d=ismism -c=fund --file=dbimport/fund.json
-mongoimport --jsonArray -d=ismism -c=fund --file=dbimport/fund.json
+echo mongoimport --jsonArray -d=ismism -c=fund --mode=upsert --file=dbimport/fund.json
+mongoimport --jsonArray -d=ismism -c=fund --mode=upsert --file=dbimport/fund.json
 
-echo mongoimport --jsonArray -d=ismism -c=dat --file=dbimport/dat.json
-mongoimport --jsonArray -d=ismism -c=dat --file=dbimport/dat.json
+echo mongoimport --jsonArray -d=test -c=dat --mode=upsert --file=dbimport/dat.json
+mongoimport --jsonArray -d=ismism -c=dat --mode=upsert --file=dbimport/dat.json
 
 mongosh ismism --eval 'db.getCollectionNames().forEach(coll => { 
     const idx = db.getCollection(coll).getIndexes()
