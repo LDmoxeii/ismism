6aed482dab76767806824a7e4d34846f3f563702
728 <septem.duo.octo@outlook.com>
2023-01-12 13:21:43 +0800

    ui/54

--- cli/build.zsh
+++ cli/build.zsh
@@ -4,6 +4,7 @@ rm cli/*.js
 rm -rf ui
 
 mkdir ui
+mkdir ui/54
 
 cd ismism.ts
 
@@ -13,6 +14,9 @@ deno bundle cli/smstst.ts ../cli/smstst.js
 
 cp ui/index.html ../ui/index.html
 deno bundle ui/bind.ts ../ui/bind.js
+
+cp ui/54/index.html ../ui/54/index.html
+cp ui/54/v1.pdf ../ui/54/v1.pdf
 
 # mkdir ../ui/cast
 # cp ui/cast/index.html ../ui/cast/index.html
