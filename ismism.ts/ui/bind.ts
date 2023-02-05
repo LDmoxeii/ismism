c090677f4a786b86c343ac575a7c4e860381e7a8
728 <septem.duo.octo@outlook.com>
2023-02-05 12:03:56 +0800

    ui adm #39

--- ismism.ts/ui/bind.ts
+++ ismism.ts/ui/bind.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-window-prefix
-import { not_nbr } from "../src/ont/sms.ts"
+import { adm } from "../src/ont/adm.ts"
 import { utc_medium } from "../src/ont/utc.ts"
 import type { Pas } from "../src/pra/pas.ts"
 import type { PasCode, UsrAct } from "../src/pra/pos.ts"
@@ -23,25 +23,39 @@ async function pos<T>(
 	return res.json() as T
 }
 
-
 let hash = ""
 let pas: Pas | null = null
 const main = document.getElementById("main")!
+
+function selopt(
+	sel: HTMLSelectElement,
+	ts: Iterable<string>,
+) {
+	sel.options.length = 0
+	for (const t of ts) {
+		const opt = document.createElement("option")
+		opt.text = t
+		sel.add(opt)
+	}
+}
 
 function paspre(
 ) {
 	const [paspre_t, [
 		nbr_e, send_e,
+		adm_e, adm1_e, adm2_e,
 		pre_e, actid_e, act_e,
 		pas_e, code_e, issue_e,
 		hint_e,
 	]] = bind("paspre", [
 		"nbr", "send",
+		"adm", "adm1", "adm2",
 		"pre", "actid", "act",
 		"pas", "code", "issue",
 		"hint",
 	]) as [DocumentFragment, [
 		HTMLInputElement, HTMLButtonElement,
+		HTMLElement, HTMLSelectElement, HTMLSelectElement,
 		HTMLElement, HTMLInputElement, HTMLButtonElement,
 		HTMLElement, HTMLInputElement, HTMLButtonElement,
 		HTMLElement,
@@ -56,7 +70,12 @@ function paspre(
 			hint_e.innerText = `验证码已发送，可多次使用\n一小时内不再重复发送${utc}`
 			pas_e.classList.remove("none")
 		} else {
-			hint_e.innerText = `手机号未注册\n输入注册激活码\n激活码只能使用一次，确认手机号无误`
+			hint_e.innerText = `手机号未注册\n输入居住地与注册激活码\n激活码只能使用一次，确认手机号无误`
+			selopt(adm1_e, adm.keys())
+			adm1_e.value = "江苏"
+			selopt(adm2_e, adm.get("江苏")!)
+			adm1_e.addEventListener("change", () => selopt(adm2_e, adm.get(adm1_e.value)!))
+			adm_e.classList.remove("none")
 			pre_e.classList.remove("none")
 		}
 	}
@@ -64,13 +83,14 @@ function paspre(
 
 	act_e.addEventListener("click", async () => {
 		if (!actid_e.checkValidity()) { alert("无效激活码"); return }
-		actid_e.readOnly = act_e.disabled = true
-		const uid = await pos<UsrAct>("pre", { actid: actid_e.value, nbr: nbr_e.value, adm1: "江苏", adm2: "苏州" })
+		actid_e.readOnly = act_e.disabled = adm1_e.disabled = adm2_e.disabled = true
+
+		const uid = await pos<UsrAct>("pre", { actid: actid_e.value, nbr: nbr_e.value, adm1: adm1_e.value, adm2: adm2_e.value })
 		if (uid) {
 			await send()
 			pas_e.classList.remove("none")
 		} else {
-			actid_e.readOnly = act_e.disabled = false
+			actid_e.readOnly = act_e.disabled = adm1_e.disabled = adm2_e.disabled = false
 			alert("无效激活码")
 		}
 	})
