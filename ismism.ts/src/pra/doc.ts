0b2cf258318f5f86e7174e6a50ffd7d435cea457
728 <septem.duo.octo@outlook.com>
2023-02-13 14:15:23 +0800

    refactor #34

--- ismism.ts/src/pra/doc.ts
+++ ismism.ts/src/pra/doc.ts
@@ -6,13 +6,13 @@ import { sidnam, soc_r } from "../eid/soc.ts"
 import { Agd, Soc, Usr } from "../eid/typ.ts"
 import { usr_r } from "../eid/usr.ts"
 
-const projid = { nam: 1, rej: 1, ref: 1, utc: 1, adm1: 1, adm2: 1, intro: 1 } as const
+const pid = { nam: 1, rej: 1, ref: 1, utc: 1, adm1: 1, adm2: 1, intro: 1 } as const
 
 export async function usr(
 	_id: Usr["_id"],
 ) {
 	const [u, snam, nr] = await Promise.all([
-		usr_r({ _id }, projid),
+		usr_r({ _id }, pid),
 		sidnam(_id),
 		nrec({ uid: [_id] })
 	])
@@ -24,7 +24,7 @@ export async function usr(
 export async function soc(
 	_id: Soc["_id"]
 ) {
-	const s = await soc_r(_id, { ...projid, sec: 1, uid_max: 1, uid: 1, res_max: 1, res: 1 })
+	const s = await soc_r(_id, { ...pid, sec: 1, uid_max: 1, uid: 1, res_max: 1, res: 1 })
 	if (!s) return null
 	const [unam, nr] = await Promise.all([
 		idnam(coll.usr, [...s.rej, ...s.ref, ...s.sec, ...s.uid, ...s.res]),
@@ -36,7 +36,7 @@ export async function soc(
 export async function agd(
 	_id: Agd["_id"]
 ) {
-	const a = await agd_r(_id, { ...projid, detail: 1, budget: 1, fund: 1, expense: 1, goal: 1, img: 1, res_max: 1 })
+	const a = await agd_r(_id, { ...pid, detail: 1, budget: 1, fund: 1, expense: 1, goal: 1, img: 1, res_max: 1 })
 	if (!a) return null
 	const [unam, nr] = await Promise.all([
 		idnam(coll.usr, [...a.rej, ...a.ref]),
