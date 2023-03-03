4ee2d093e841540257136efd8fb520fab103c60c
728 <septem.duo.octo@outlook.com>
2023-03-03 10:49:02 +0800

    ser #40

--- cli/build.zsh
+++ cli/build.zsh
@@ -8,7 +8,7 @@ cd ismism.ts
 deno bundle src/ser.ts ../cli/ser.js
 deno bundle cli/dbset.ts ../cli/dbset.js
 
-mkdir ../ui
+# mkdir ../ui
 
-deno bundle ui/bind/bind.ts ui/bind/bind.js
-deno run --allow-all ui/ui.ts
+# deno bundle ui/bind/bind.ts ui/bind/bind.js
+# deno run --allow-all ui/ui.ts
