f4701737f963d9133b3cb4054ed398b2ba45a86a
728 <septem.duo.octo@outlook.com>
2023-03-21 12:37:20 +0800

    deploy #41

--- cli/remote/deploy.zsh
+++ cli/remote/deploy.zsh
@@ -1,3 +1,4 @@
+source .zshrc
 rm -rf ismism/cli
 rm -rf ismism/ui/index.html
 rm -rf ismism/dbimport
@@ -7,3 +8,11 @@ unzip -o ismism.zip -d ismism
 
 mkdir -p ismism-old
 mv ismism.zip  "ismism-old/ismism-$(date +%Y%m%d-%H%M).zip"
+cd ismism
+
+curl http://localhost:728/quit
+nohup deno run --allow-net --allow-read cli/ser.js > log/ismism.log &
+
+tail log/mongo.log log/access.log log/error.log log/nginx.log log/mongod.log log/ismism.log
+echo "\nismism released\n"
+pgrep -lf nginx; pgrep -lf mongo; pgrep -lf "deno run"
