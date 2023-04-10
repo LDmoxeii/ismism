dd9c9e92b64631931f61c088096d7635fd0eed0b
728 <septem.duo.octo@outlook.com>
2023-04-10 17:01:40 +0800

    bump

--- ismism.ts/ui/bind/nav.ts
+++ ismism.ts/ui/bind/nav.ts
@@ -127,7 +127,7 @@ window.addEventListener("scroll", () => {
 
 export async function load(
 ) {
-	console.log("ismism-20230319")
+	console.log("ismism-20230410")
 	console.log(`\n主义主义开发小组！成员招募中！\n\n发送自我介绍至网站维护邮箱，或微信联系 728 万大可\n \n`)
 	await navpas()
 	window.dispatchEvent(new Event("hashchange"))
