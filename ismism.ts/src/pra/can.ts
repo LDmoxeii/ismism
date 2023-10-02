4a1ebf994580ca3bf1fa238c6dc8574b25a188a5
728 <septem.duo.octo@outlook.com>
2023-10-02 16:49:57 +0800

    can #43

---
+++ ismism.ts/src/pra/can.ts
@@ -0,0 +1,75 @@
+import type { Soc } from "../eid/typ.ts"
+import { is_aut } from "../eid/is.ts"
+import { Pas } from "./pas.ts"
+
+// deno-lint-ignore no-explicit-any
+export type Ret<T extends (...args: any) => any> = Awaited<ReturnType<T>>
+
+function is_soc(
+	sl: Soc["_id"][],
+	s?: Soc["_id"],
+): boolean {
+	if (!s) return sl.length > 0
+	return sl.includes(s)
+}
+export function is_cdt(
+	pas: Pas,
+	soc?: Soc["_id"],
+): boolean {
+	return is_soc(pas.cdt, soc)
+}
+export function is_sec(
+	pas: Pas,
+	soc?: Soc["_id"],
+): boolean {
+	return is_soc(pas.sec, soc)
+}
+
+export function is_pre_usr(
+	pas: Pas
+): boolean {
+	return is_aut(pas.aut) || is_sec(pas)
+}
+export function is_pre_soc(
+	pas: Pas
+): boolean {
+	return is_aut(pas.aut, "aut")
+}
+export function is_pre_agd(
+	pas: Pas
+): boolean {
+	return is_sec(pas)
+}
+export function is_pre_cdt(
+	pas: Pas,
+	soc: Soc["_id"],
+): boolean {
+	return is_sec(pas, soc)
+}
+export function is_pre_dbt(
+	pas: Pas,
+	soc: Soc["_id"],
+): boolean {
+	return is_cdt(pas, soc)
+}
+export function is_pre_ern(
+	pas: Pas,
+	soc: Soc["_id"],
+): boolean {
+	return is_sec(pas, soc)
+}
+export function is_pre_wsl(
+	pas: Pas
+): boolean {
+	return is_aut(pas.aut, "wsl")
+}
+export function is_pre_lit(
+	pas: Pas
+): boolean {
+	return is_aut(pas.aut, "lit")
+}
+export function is_pre_aut(
+	pas: Pas
+): boolean {
+	return is_aut(pas.aut, "sup")
+}
