f75d271dd03c93a6a232c9069a219fb89c69e065
LD_moxeii <2649075705@qq.com>
2024-04-21 17:52:06 +0800

    utc 时间格式 #45

---
+++ src/ont/utc.ts
@@ -0,0 +1,30 @@
+export const utc_m = 60 * 1000
+export const utc_h = 60 * utc_m
+export const utc_d = 24 * utc_h
+
+export const utc_oft = new Date().getTimezoneOffset() * utc_m
+const locale_cn = "zh-CN"
+
+export function utc_dt(
+    utc: number,
+    dt: "short" | "medium" | "pad" | "padutc" = "short",
+): string {
+    const t = new Date(utc)
+    switch (dt) {
+        default: case "short": return t.toLocaleString(locale_cn, { dateStyle: "short", timeStyle: "short" }).replaceAll("/", "-")
+        case "medium": return t.toLocaleString(locale_cn, { dateStyle: "medium", timeStyle: "short" }).replaceAll("/", "-")
+        case "pad": {
+            const y = t.getFullYear()
+            const m = `${t.getMonth() + 1}`.padStart(2, "0")
+            const d = `${t.getDate()}`.padStart(2, "0")
+            return `${y}-${m}-${d}`
+        }
+        case "padutc": {
+            const y = t.getUTCFullYear()
+            const m = `${t.getUTCMonth() + 1}`.padStart(2, "0")
+            const d = `${t.getUTCDate()}`.padStart(2, "0")
+            console.log(y, m, d)
+            return `${y}-${m}-${d}`
+        }
+    }
+}
--- tst/ont.test.ts
+++ tst/ont.test.ts
@@ -1,5 +1,6 @@
 import { is_adm } from "../src/ont/adm.ts";
 import { frm_base64, frm_hex, frm_u8, to_base64, to_hex, to_u8 } from "../src/ont/base.ts";
+import { utc_dt } from "../src/ont/utc.ts";
 import { assertEquals } from "./mod.test.ts";
 
 Deno.test("base", () => {
@@ -16,6 +17,15 @@ Deno.test("base", () => {
     const b16 = to_hex(b)
     assertEquals(b, frm_hex(b16))
     assertEquals(len * 2, b16.length)
+})
+
+Deno.test("utc", () => {
+    const short = "2023-4-18 04:35"
+    const utc = new Date(short).getTime()
+    assertEquals(short, utc_dt(utc))
+    assertEquals("2023年4月18日 04:35", utc_dt(utc, "medium"))
+    assertEquals("2023-04-18", utc_dt(utc, "pad"))
+    assertEquals("2023-04-17", utc_dt(utc, "padutc"))
 })
 
 
